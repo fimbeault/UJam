2b652cb633c1f6bda2a14d5ea2e07b6956de4b25
fimbeault <fimbeault@gmail.com>
2015-02-28 10:35:46 -0500

    SongParser!

--- Assets/Scripts/Rythmic/SongManager.cs
+++ Assets/Scripts/Rythmic/SongManager.cs
@@ -3,11 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class SongManager : MonoBehaviour {
-
+ 
 	static private List<Combo> comboList = new List<Combo>();
 
 	static public Combo GetNextCombo()
 	{
+		if (comboList == null)
+			return null;
+
 		if (comboList.Count > 0) {
 			Combo combo = comboList[0];
 			comboList.RemoveAt(0);
@@ -19,15 +22,8 @@ public class SongManager : MonoBehaviour {
 	}
 
 	// Use this for initialization
-	void Init () {
-		Combo combo = new Combo();
-
-		for (int i = 0; i < 10; ++i) {
-			Note note = new Note();
-			note.fTime = (float)i;
-
-			combo.notes.Add(note);
-		}
+	void Awake () {
+		comboList = SongParser.ParseSong (Application.dataPath + "/Data/SongsDefinition/Song1.xml");
 	}
 	
 	// Update is called once per frame
---
+++ Assets/Scripts/Rythmic/SongParser.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+
+public class SongParser {
+
+	static public List<Combo> ParseSong(string _file)
+	{
+		List<Combo> comboList = new List<Combo> ();
+
+		XmlDocument xml = new XmlDocument ();
+		xml.Load (_file);
+
+		XmlNodeList xmlSections = xml.SelectNodes ("//Section");
+
+		foreach (XmlNode xmlSection in xmlSections)
+		{
+			XmlNodeList xmlCombos = xmlSection.SelectNodes("Combo");
+
+			foreach (XmlNode xmlCombo in xmlCombos)
+			{
+				Combo combo = new Combo();
+
+				XmlNodeList xmlNotes = xmlCombo.SelectNodes("Note");
+
+				foreach (XmlNode xmlNote in xmlNotes)
+				{
+					Note note = new Note();
+					float.TryParse(xmlNote.Attributes.GetNamedItem("time").Value, out note.fTime);
+
+					combo.notes.Add(note);
+				}
+
+				if (combo.notes.Count > 0)
+					comboList.Add (combo);
+			}
+		}
+
+		return comboList;
+	}
+}
