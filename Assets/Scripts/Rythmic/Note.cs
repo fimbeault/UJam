d596d832a55cef50f87c6b5309d0b543f15573a2
fimbeault <fimbeault@gmail.com>
2015-02-28 15:15:20 -0500

    Fix build

--- Assets/Scripts/Rythmic/Note.cs
+++ Assets/Scripts/Rythmic/Note.cs
@@ -2,6 +2,18 @@
 using System.Collections;
 
 public class Note {
+
+	public enum NoteTime
+	{
+		NoteType_Whole,
+		NoteType_Half,
+		NoteType_Quarter,
+		NoteType_Eighth,
+		NoteType_Sixteenth,
+		NoteType_ThirtySecond,
+		NoteType_SixtyFourth
+	};
+
     public string sName;
 	public float fTime;
 
