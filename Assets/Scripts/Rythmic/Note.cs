0d72b725d5f17efa56bc6b49746eea9b041b8a1e
fimbeault <fimbeault@gmail.com>
2015-02-28 23:27:08 -0500

    Fix décalage des notes eighth et plus rapides

--- Assets/Scripts/Rythmic/Note.cs
+++ Assets/Scripts/Rythmic/Note.cs
@@ -5,13 +5,13 @@ public class Note {
 
 	public enum NoteTime
 	{
-		NoteType_Whole,
-		NoteType_Half,
-		NoteType_Quarter,
-		NoteType_Eighth,
-		NoteType_Sixteenth,
-		NoteType_ThirtySecond,
-		NoteType_SixtyFourth
+		NoteType_Whole = 1,
+		NoteType_Half = 2,
+		NoteType_Quarter = 4,
+		NoteType_Eighth = 8,
+		NoteType_Sixteenth = 16,
+		NoteType_ThirtySecond = 32,
+		NoteType_SixtyFourth = 64
 	};
 
     public string sType;
