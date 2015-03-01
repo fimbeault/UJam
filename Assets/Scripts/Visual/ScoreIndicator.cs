fd30b156d3283693ed819d31fe4645eaeffac83f
fimbeault <fimbeault@gmail.com>
2015-03-01 09:55:22 -0500

    WIP scoreIndicator

---
+++ Assets/Scripts/Visual/ScoreIndicator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreIndicator : MonoBehaviour {
+
+	const float fScoreMax = 5000.0f;
+
+	const float fYMin = -5.0f;
+	const float fYAwsomeZoneStart = 3.3f;
+	const float fYMax = 5.2f;
+
+	float fCurrentScore = 0.0f;
+
+	public void ResetScore()
+	{
+		fCurrentScore = 0.0f;
+	}
+
+	public void AddScore()
+	{
+	}
+}
